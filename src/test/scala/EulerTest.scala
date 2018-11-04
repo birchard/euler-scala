0f187910ee8990d9a903fba93bf75020b784d54b
birch hayes <zornag@gmail.com>
2018-11-04 15:31:49 -0600

    sum of even fibonacci less than 4,000,000

--- src/test/scala/EulerTest.scala
+++ src/test/scala/EulerTest.scala
@@ -9,4 +9,8 @@ class EulerTest extends FunSuite with Matchers {
   test("multiples of 3 and 5 up to 1000") {
     Euler.sumOfMultiples(1000) should be (233168)
   }
+
+  test("sum of even fibonacci numbers less than 4,000,000") {
+    Euler.sumOfEvenFib should be (4613732)
+  }
 }
