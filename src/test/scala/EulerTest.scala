50119b0c4597657c9c9ba23d545a744a9a919b44
birch hayes <zornag@gmail.com>
2019-04-13 15:03:21 -0500

    wip

--- src/test/scala/EulerTest.scala
+++ src/test/scala/EulerTest.scala
@@ -13,4 +13,12 @@ class EulerTest extends FunSuite with Matchers {
   test("sum of even fibonacci numbers less than 4,000,000") {
     Euler.sumOfEvenFib should be (4613732)
   }
+
+  test("largest prime factor of 13195 is 29") {
+    Euler.largestPrimeFactor(13195) should be (29)
+  }
+
+  test("largest prime factor of 600851475143 is 29") {
+    Euler.largestPrimeFactor(600851475143L) should be (6857)
+  }
 }
